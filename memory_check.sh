ce33639a7c6380d91cc23170b106377047526199
Richard Lozano <87725751+rvlozano@users.noreply.github.com>
2024-06-10 12:11:16 -0700

    Update memory_check.sh

--- memory_check.sh
+++ memory_check.sh
@@ -6,7 +6,7 @@ total=$(free | grep Mem: | awk '{print $2}')
 used=$(free | grep Mem: | awk '{print $3}')
 
 percent=$(echo "print(int(($used/$total)*100))" | python3) # Uses Python3
-# percent=$(echo "scale=2; ($used / $total) * 100" | bc)   # Uses BC 
+# percent=$(echo "scale=2; ($used / $total) * 100" | bc)   # Uses BC most linux distros have this by default
 
 if [[ $percent > 95 ]]
 then
